598ba36434a85eea6eda2f74d411574e40f75d76
Orderr147 <131309176+Orderr147@users.noreply.github.com>
2023-07-20 17:47:13 +0300

    Update Megamenu.jsx

--- frontend/src/components/home/Megamenu.jsx
+++ frontend/src/components/home/Megamenu.jsx
@@ -5,12 +5,12 @@ import React from 'react'
 const Megamenu = () => {
   return (
     <Box display={{ base: 'none', lg: 'flex' }} gap='5'>
-      <Link to='/cart'>cart</Link>
+      <Link to='/cart'>السله</Link>
       <Link to='/signup'>register</Link>
       <Link to='/login'>login</Link>
-      <Link to='/myaccount'>myaccount</Link>
-      <Link to='/product'>product</Link>
-      <Link to='/adminlogin'>admin</Link>
+      <Link to='/myaccount'>الملف الشخصي </Link>
+      <Link to='/product'>المنتجات </Link>
+      <Link to='/adminlogin'>مسئول</Link>
     </Box>
   )
 }
